763581ce89ff4284d3835a7ac5d99f47b074e083
JenniferAlbornoz <jc.albornoz.ortiz@gmail.com>
2025-07-10 02:59:28 -0400

    actualizo producto agregando dto categoria

--- usuario/src/test/java/com/prueba3/usuario/controller/UsuarioControllerTest.java
+++ usuario/src/test/java/com/prueba3/usuario/controller/UsuarioControllerTest.java
@@ -2,9 +2,12 @@ package com.prueba3.usuario.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.prueba3.usuario.dto.UsuarioDTO;
+import com.prueba3.usuario.service.UsuarioService;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -20,6 +23,9 @@ class UsuarioControllerTest {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @MockBean
+        private UsuarioService usuarioService;
 
     @Test
     void crearUsuario_emailVacio_retorna400() throws Exception {
